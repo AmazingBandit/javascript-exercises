813d281f2b2cca04d8189dfd55109bd8b01543ba
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-22 14:56:19 -0700

    Finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // (F-32)*(5/9) = C
+  if ((fahrenheit-32)*(5/9)%1 == 0) {
+    return celsius = +((fahrenheit-32)*(5/9)).toFixed(0); 
+  } else {
+  return celsius = +((fahrenheit-32)*(5/9)).toFixed(1); }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // (C*(9/5))+32 = F
+  if(((celsius*(9/5))+32)%1 == 0) {
+  return fahrenheit = +((celsius*(9/5))+32).toFixed(0);
+  } else {
+    return fahrenheit = +((celsius*(9/5))+32).toFixed(1)
+  }
 };
 
 // Do not edit below this line
