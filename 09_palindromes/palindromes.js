623f1ea81d4c8cfb19c4fa7c76d08a1ab213001a
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-25 20:14:25 -0700

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (startWord) {
+    startWord = startWord
+        .replace(/[^A-Za-z0-9]/gi, '')
+        .toLowerCase();
 
+    const endWord = startWord
+        .split('')
+        .reverse()
+        .join('')
+        .replace(/[^A-Za-z0-9]/gi, '')
+        .toLowerCase();
+
+    if (startWord === endWord) return true;
+    else return false;
 };
 
 // Do not edit below this line
