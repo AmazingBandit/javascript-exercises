f9bdda484adf47f8718de3e53a0783e706e0fcb2
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-25 19:40:34 -0700

    Completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0) {
+    return 0; 
+  }
+  else {
+	let total = array.reduce((accumulator, currentValue) => 
+    accumulator + currentValue);
+    return total;
+  }
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  return args.reduce(function (accumulator, currentValue) {
+    return accumulator * currentValue;
+  })
+};
+multiply(2,4);
+multiply(2,4,6,8,10,12,14)
 
+const power = function(x, y) {
+  return x**y;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+  if(x == 0) {
+    return 1;
+  } else {
+    for(i = 1; i <= x; i++) {
+       total *= i; 
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
