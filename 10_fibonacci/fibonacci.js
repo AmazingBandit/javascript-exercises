8c1c20473715d3ffd60d8709d2c6cf3ee26fcbc6
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-29 18:06:35 -0700

    Finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    const array = [];
+    if(n<0) {
+        return "OOPS";
+    } else {
+    for (i = 0; i<n; i++) {
+            if (array.length === 0) {
+                array.push(0);
+                array.push(1);
+            }
+            array.push(array[i] + array[i+1]);
+        }
+    }
+    return array[n];
 };
+
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
