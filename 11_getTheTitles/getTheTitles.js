95be4dc356e0287ba80c4fbf069d874c2175f102
AmazingBandit <sa.rajbhandari@gmail.com>
2023-07-29 19:14:26 -0700

    Finished exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  const newArray = [];
+  for (i = 0; i < array.length; i++) {
+    newArray.push(array[i].title);
+  }
+  return newArray;
 };
 
 // Do not edit below this line
