8cafdca2cfbc5891f1efe2126739bd695ecf04dc
AmazingBandit <sa.rajbhandari@gmail.com>
2023-08-11 10:37:21 -0700

    Fixing broken code for findTheOldest() function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  }
+  
+];
 
+const findTheOldest = function () {
+  let output = "";
+  people.reduce((p, c, i, a) => {
+    const age = c.yearOfDeath - c.yearOfBirth; 
+    if (age > p) {
+      if (i == a.length - 1) {
+        output = c.name;
+      } else {
+        output = c.name;
+        return age;
+      } 
+      return output;
+    } 
+    return output;
+  }, 0);
 };
+
+findTheOldest();
 
 // Do not edit below this line
 module.exports = findTheOldest;
