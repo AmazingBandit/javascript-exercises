0b1f30a4cbcb824945a77e9ad4924d6c2411c0b6
AmazingBandit <sa.rajbhandari@gmail.com>
2023-08-23 01:15:45 -0700

    Finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,40 +1,17 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  }
-  
-];
+const findTheOldest = function(array) {
+  return array.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+  return oldestAge > currentAge ? oldestPerson : currentPerson;
+});
+}
 
-const findTheOldest = function () {
-  let output = "";
-  people.reduce((p, c, i, a) => {
-    const age = c.yearOfDeath - c.yearOfBirth; 
-    if (age > p) {
-      if (i == a.length - 1) {
-        output = c.name;
-      } else {
-        output = c.name;
-        return age;
-      } 
-      return output;
-    } 
-    return output;
-  }, 0);
+const getAge = function(birth, death) {
+  if(!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
-findTheOldest();
-
-// Do not edit below this line
 module.exports = findTheOldest;
+
