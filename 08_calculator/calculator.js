be7b44dd2ff585dcc541cb9eae416cfad95f8d31
AmazingBandit <sa.rajbhandari@gmail.com>
2023-12-19 20:00:03 -0800

    rewrote logic for all module and merged re-practice-08-to-09 to main

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,46 +1,78 @@
-const add = function(x,y) {
-	return x+y;
-};
+// const add = function(x,y) {
+// 	// return x+y;
+// };
 
-const subtract = function(x,y) {
-	return x-y;
-};
+// const subtract = function(x,y) {
+// 	// return x-y;
+// };
 
-const sum = function(array) {
-  if(array.length == 0) {
-    return 0; 
-  }
-  else {
-	let total = array.reduce((accumulator, currentValue) => 
-    accumulator + currentValue);
-    return total;
-  }
-};
+// const sum = function(array) {
+//   // if(array.length == 0) {
+//   //   return 0;
+//   // }
+//   // else {
+// 	// let total = array.reduce((accumulator, currentValue) =>
+//   //   accumulator + currentValue);
+//   //   return total;
+//   // }
+// };
 
-const multiply = function(...args) {
-  return args.reduce(function (accumulator, currentValue) {
-    return accumulator * currentValue;
-  })
-};
-multiply(2,4);
-multiply(2,4,6,8,10,12,14)
+// const multiply = function(...args) {
+//   return args.reduce(function (accumulator, currentValue) {
+//     return accumulator * currentValue;
+//   })
+// };
+// multiply(2,4);
+// multiply(2,4,6,8,10,12,14)
 
-const power = function(x, y) {
-  return x**y;
-};
+// const power = function(x, y) {
+//   return x**y;
+// };
 
-const factorial = function(x) {
-  let total = 1;
-  if(x == 0) {
-    return 1;
-  } else {
-    for(i = 1; i <= x; i++) {
-       total *= i; 
-    }
-  }
+// const factorial = function(x) {
+//   let total = 1;
+//   if(x == 0) {
+//     return 1;
+//   } else {
+//     for(i = 1; i <= x; i++) {
+//        total *= i;
+//     }
+//   }
+//   return total;
+// };
+
+const add = (x, y) => x + y;
+
+const subtract = (x, y) => x - y;
+
+const sum = (array) => {
+  let total = 0;
+  array.forEach((elem) => {
+    total += elem;
+  });
   return total;
 };
+console.log(sum([7, 11]));
 
+const multiply = (...args) => args.reduce((acc, curr) => acc * curr);
+console.log(multiply(2, 4, 6, 8, 10, 12, 14));
+
+const power = (x, y) => x ** y;
+
+const factorial = (x) => {
+  const newArray = [];
+  if (x == 0) {
+    return 1;
+  }
+  for (i = x; i > 0; i--) {
+    newArray.push(i);
+  }
+  return newArray.reduce((prev, curr) => {
+    return prev * curr;
+  });
+};
+
+console.log(factorial(0));
 // Do not edit below this line
 module.exports = {
   add,
@@ -48,5 +80,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
